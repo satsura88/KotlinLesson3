738327b821de66e0b21f9d663c83b8f25c09764a
satsura88 <satsura88@gmail.com>
2022-07-02 15:57:52 +0300

    примеры Nullable и NotNull

---
+++ app/src/main/java/ru/geekbrains/kotlinlesson3/Test.java
@@ -0,0 +1,20 @@
+package ru.geekbrains.kotlinlesson3;
+
+import androidx.annotation.NonNull;
+
+public class Test {
+    void foo(@NonNull String s) {
+
+    }
+
+    Integer i;
+    int i2;
+
+    String fooRun() {
+        return "";
+    }
+
+    String serverResponse() {
+        return "";
+    }
+}
